c2a9abd19f4e96e2b3fd34e1e3792fdc5c6925c2
Jhonatec <jhonsreis@gmail.com>
2024-02-01 14:57:08 -0300

    feat: README for keyboard test

---
+++ 003/README.md
@@ -0,0 +1,46 @@
+# Teste de Automação usando teclado
+
+Um teste usando apenas duas libs para automatizar a busca pelo app "Safari" e abrir um link repetidas vezes
+
+## Configuração do Ambiente
+
+### 1. Ambiente Virtual (venv)
+
+Certifique-se de ter o Python instalado. Em seguida, crie e ative um ambiente virtual:
+
+```bash
+# No diretório do seu projeto
+python -m venv venv   #Cria o ambiente virtual
+```
+
+- Windows:
+
+```bash
+  venv\Scripts\activate
+```
+
+- macOS/Linux:
+
+```bash
+  source venv/bin/activate
+```
+
+### 2. Instalação de Dependências
+
+Após ativar o ambiente virtual, instale as dependências do projeto usando o arquivo requirements.txt:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Executando o Projeto
+
+Para executar, apenas rode o arquivo <code>concepts.py</code> da seguinte maneira:
+
+```bash
+python3 concepts.py
+```
+
+
+
+Aguarde a mágica acontecer!
--- 003/concepts.py
+++ 003/concepts.py
@@ -2,43 +2,41 @@ import keyboard
 
 import time
 
-# Press and release Command (Cmd) key
-# keyboard.write("ping google.com")
-# keyboard.press_and_release("enter")
-
-# Abrir o Chrome
+MAX = 5
+# Abrir o Safari
 
 keyboard.press("command")
 keyboard.press("space")
 keyboard.release("space")
 keyboard.release("command")
-keyboard.write("chrome")
+keyboard.write("safari")
 keyboard.press_and_release("enter")
 
 
-# Abrir uma nova aba
-keyboard.press("command")
-keyboard.press("shift")
-keyboard.press_and_release("n")
-keyboard.release("shift")
-keyboard.release("command")
-
 for i in range(2):
-    for i in range(10):
+    # Abrir uma nova aba anônima
+    keyboard.press("command")
+    keyboard.press("shift")
+    keyboard.press_and_release("n")
+    keyboard.release("shift")
+    keyboard.release("command")
+    for i in range(MAX):
         time.sleep(1)
         # Abrir meu vídeo no youtube hehehehe
         keyboard.write("https://www.youtube.com/watch?v=Uzuh1LpAKuw")
         keyboard.press_and_release("enter")
         time.sleep(1)
+        if i < (MAX - 1):
+            # Abrir mais uma aba
+            keyboard.press("command")
+            keyboard.press_and_release("t")
+            keyboard.release("command")
 
-        # Abrir mais uma aba
-        keyboard.press("command")
-        keyboard.press_and_release("t")
-        keyboard.release("command")
-
+    # esperar antes de fechar as abas
     time.sleep(5)
 
-    for i in range(9):
+    for i in range(MAX):
+        time.sleep(1)
         keyboard.press("command")
         keyboard.press_and_release("w")
         keyboard.release("command")
