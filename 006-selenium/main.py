4d0e4ad40a9523e39f803d15f628cdf2a73b44df
Jhonatec <jhonsreis@gmail.com>
2024-02-02 21:12:11 -0300

    feat: selenium with automatic chrome driver

--- 006-selenium/main.py
+++ 006-selenium/main.py
@@ -1,9 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-navegador = webdriver.Chrome()
-navegador.get("https://www.google.com")
+try:
+    servico = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=servico)
+    driver.get("https://www.jhonatec.com")
 
-print(navegador.title)
+    print(driver.title)
 
-time.sleep(5)
+    # procurar link de Atendimento
+    atendimento = driver.find_element(by="link text", value="Atendimento")
+    atendimento.click()
+
+    while True:
+        # Adicione outras ações ou manipulações aqui, se necessário
+        time.sleep(1)  # Um pequeno atraso para não sobrecarregar a CPU
+
+except KeyboardInterrupt:
+    print("\nScript interrompido pelo usuário")
+
+finally:
+    driver.quit()  # Certifique-se de fechar o navegador ao finalizar o script
