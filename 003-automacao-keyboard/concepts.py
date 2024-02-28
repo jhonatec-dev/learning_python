96e674f03ff561e919dba067564c8de7367b8cec
Jhonatec <jhonsreis@gmail.com>
2024-02-28 11:21:27 -0300

    feat: eternal automation on youtube

--- 003-automacao-keyboard/concepts.py
+++ 003-automacao-keyboard/concepts.py
@@ -1,20 +1,20 @@
+import random
 import keyboard
 
 import time
 
 MAX = 5
+
 # Abrir o Safari
-
-keyboard.press("command")
-keyboard.press("space")
-keyboard.release("space")
-keyboard.release("command")
-keyboard.write("safari")
-keyboard.press_and_release("enter")
-
-
-for i in range(15):
-    time.sleep(3)
+while True:
+    time.sleep(2)
+    keyboard.press("command")ew
+    keyboard.press("space")
+    keyboard.release("space")
+    keyboard.release("command")
+    keyboard.write("safari")
+    keyboard.press_and_release("enter")
+    time.sleep(2)
     # Abrir uma nova aba anônima
     keyboard.press("command")
     keyboard.press("shift")
@@ -26,18 +26,15 @@ for i in range(15):
         # Abrir meu vídeo no youtube hehehehe
         keyboard.write("https://www.youtube.com/watch?v=Uzuh1LpAKuw")
         keyboard.press_and_release("enter")
-        time.sleep(20)
+        time.sleep(random.randint(15, 55))
         if i < (MAX - 1):
             # Abrir mais uma aba
             keyboard.press("command")
             keyboard.press_and_release("t")
             keyboard.release("command")
-
     # esperar antes de fechar as abas
-    time.sleep(600)
-
-    for i in range(MAX):
-        time.sleep(1)
-        keyboard.press("command")
-        keyboard.press_and_release("w")
-        keyboard.release("command")
+    time.sleep(random.randint(150, 550))
+    time.sleep(1)
+    keyboard.press("command")
+    keyboard.press_and_release("q")
+    keyboard.release("command")
