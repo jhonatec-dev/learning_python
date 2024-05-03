39547a3c1f0d9ee34f8815839209857188191d3a
JhonaTec <jhonsreis@gmail.com>
2024-05-03 09:56:51 -0300

    fix get_from_amazon

--- 015-HTTP/fromSoup.py
+++ 015-HTTP/fromSoup.py
@@ -5,25 +5,37 @@ import json
 
 def get_from_amazon(soup: BeautifulSoup):
     try:
+        print("Reading from Amazon")
+        # finalPrice = soup.find(class_="a-price-whole")
+        # print("finalPrice", finalPrice)
+        # print("finalPrice.texte", finalPrice.text)
+        # priceFratcion = soup.find(class_="a-price-fraction")
+        # print("priceFratcion", priceFratcion)
+        # print("priceFratcion.text", priceFratcion.text)
+        # regularPrice = soup.find(class_="best-offer-name")
+        # print("regularPrice", regularPrice)
+        # print("regularPrice.text", regularPrice.text)
+
         finalPrice = soup.find(class_="a-price-whole").text
         finalPrice += soup.find(class_="a-price-fraction").text
-        regularPrice = soup.find(class_="best-offer-name").text
-
-        print(f"finalPrice: {finalPrice} | regularPrice: {regularPrice}")
 
         # convert the string R$ 1.000,00 to 1000.00
         finalPrice = finalPrice.replace("R$", "")
         finalPrice = finalPrice.replace(".", "")
         finalPrice = finalPrice.replace(",", ".")
+        print("finalPrice after regex", finalPrice)
         finalPrice = float(finalPrice)
 
-        # extract the price from the string including the decimal part
-        regularPrice = re.findall(r"\d+\.?\d+\,\d+", regularPrice)
-        print(regularPrice)
-        regularPrice = regularPrice[0]
-        regularPrice = regularPrice.replace(".", "")
-        regularPrice = regularPrice.replace(",", ".")
-        regularPrice = float(regularPrice)
+        try:
+            regularPrice = soup.find(class_="best-offer-name").text
+            # extract the price from the string including the decimal part
+            regularPrice = re.findall(r"\d+\.?\d+\,\d+", regularPrice)
+            regularPrice = regularPrice[0]
+            regularPrice = regularPrice.replace(".", "")
+            regularPrice = regularPrice.replace(",", ".")
+            regularPrice = float(regularPrice)
+        except Exception:
+            regularPrice = 0
 
         # If the price is minor, than has no taxes
         if regularPrice < finalPrice:
@@ -35,8 +47,11 @@ def get_from_amazon(soup: BeautifulSoup):
         }
 
     except Exception as e:
-        print(f"Erro Amazon: {e}")
-        return None
+        print(f"\nErro Amazon: {e}")
+        return {
+            "finalPrice": 9999.99,
+            "regularPrice": 9999.99,
+        }
 
 
 def get_from_kabum(soup: BeautifulSoup):
--- 015-HTTP/getItems.py
+++ 015-HTTP/getItems.py
@@ -8,19 +8,25 @@ import requests
 from fromSoup import get_from_amazon, get_from_kabum, get_from_mercado_livre
 
 API_URL = "https://api.jhonatec.com"
+API_KEY = """eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+.eyJfaWQiOiI2NTkzMjk2MTdhMzBiMDY5MGQ1NmUyNzUiLCJlbWF
+pbCI6ImNvbnRhdG9AamhvbmF0ZWMuY29tIiwicm9sZSI6ImFkbWluIiw
+ibmFtZSI6IkFkbWluIEpob25hdGVjIiwicGhvbmUiOiIzMzk5NzAyNjg2
+OSIsImlhdCI6MTcxNDU3MTMzOCwiZXhwIjoxNzE3MTYzMzM4fQ
+.luneP7zvRW8RyEILWY7r741KoCkU0FYoOpvuCg0ydIE"""
 
 
 def bypass_amazon_captcha(driver: WebDriver):
     try:
+        input = driver.find_element(By.ID, "captchacharacters")
         captcha = AmazonCaptcha.fromdriver(driver=driver)
         solved = AmazonCaptcha.solve(captcha)
-        input = driver.find_element(By.ID, "captchacharacters")
         input.clear()
         input.send_keys(solved)
         input.send_keys(Keys.RETURN)
         time.sleep(2)
-    except Exception as e:
-        print(f"Erro Amazon Captcha: {e}")
+    except Exception:
+        print("\nERROR: Amazon Captcha")
         return None
 
 
@@ -38,43 +44,27 @@ def file_already_exists(filename):
 
 def get_price(driver: WebDriver, link, item_id):
     try:
-
-        # filename = link["_id"] + ".html"
-        # checa se o arquivo existe
-        # if not file_already_exists(filename):
-        #     driver.get(link["url"])
-        #     time.sleep(2)
-        #     if link["idSeller"]["name"] == "Amazon":
-        #         bypass_amazon_captcha(driver)
-        #     with open(filename, "w", encoding="utf-8") as file:
-        #         file.write(driver.page_source)
-
-        # with open(filename, "r", encoding="utf-8") as file:
-        #     soup = BeautifulSoup(file, "html.parser")
-
-        driver.get(link["url"])
-        time.sleep(2)
-        if link["idSeller"]["name"] == "Amazon":
-            bypass_amazon_captcha(driver)
-
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-
-        # CASE link["idSeller"]["name"]
         seller = link["idSeller"]["name"]
 
-        if seller == "Kabum":
-            result = get_from_kabum(soup)
-        elif seller == "Amazon":
+        print(f"\nItem: {item_id} | Seller: {seller}")
+        driver.get(link["url"])
+        time.sleep(2.3)
+        if link["idSeller"]["name"] == "Amazon":
+            bypass_amazon_captcha(driver)
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+
+        if seller == "Amazon":
             result = get_from_amazon(soup)
+        elif seller == "Kabum":
+            result = get_from_kabum(soup)
         elif seller == "Mercado Livre":
             result = get_from_mercado_livre(soup)
         else:
             return None
 
-        print(f"Item: {item_id} | Seller: {seller}")
         compare_prices(link, item_id, result)
     except Exception as e:
-        print(f"Erro ao acessar a página: {e}")
+        print(f"\nErro ao acessar a página: {e}")
         return None
 
 
@@ -109,19 +99,24 @@ def compare_prices(link, item_id, result):
         payload["url"] = link["url"]
         payload["idSeller"] = link["idSeller"]["_id"]
         payload["_id"] = link["_id"]
-        print(f"\n\nPayload ${item_id}: {payload}\n\n")
+        print(f"\nPayload ${item_id}: {payload}\n")
         post_new_link(payload, item_id)
     except Exception as e:
-        print(f"Erro ao comparar os preços: {e}")
+        print(f"\nErro ao comparar os preços: {e}")
         return None
 
 
 def post_new_link(payload, item_id):
     try:
         response = requests.post(
-            f"{API_URL}/scrapping/item/{item_id}/link", json=payload
+            f"{API_URL}/scrapping/item/{item_id}/link",
+            json=payload,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {API_KEY}",
+            },
         )
         print(f"Item: {item_id} | Status Code: {response.status_code}")
     except Exception as e:
-        print(f"Erro ao postar o link: {e}")
+        print(f"\nErro ao postar o link: {e}")
         return None
